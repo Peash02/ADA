c0849901fd404537d01e208ad139893d8bbff248
Peash <107058247+Peash02@users.noreply.github.com>
2026-02-20 11:50:19 +0530

    Add files via upload

---
+++ heap.py
@@ -0,0 +1,32 @@
+def max_heapify(arr,n,i):
+    largest = i
+    left = 2* i + 1
+    right = 2 * i + 2
+    if left < n and arr[left] > arr[largest] :
+        largest = left
+    if right < n and arr[right] > arr[largest] :
+        largest = right
+
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        max_heapify(arr,n,largest)
+
+def build_max_heap(arr):
+    n = len(arr)
+    for i in range(n//2-1,-1,-1):
+        max_heapify(arr,n,i)
+
+def heapsort(arr):
+    n = len(arr)
+    build_max_heap(arr)
+
+    for i in range(n-1,0,-1):
+        arr[i],arr[0] = arr[0],arr[i]
+        max_heapify(arr,i,0)
+    return arr
+
+if __name__ == "__main__":
+    data = [64,34,25,12,22,11,90]
+    print("Original Array:",data)
+    print("Sorted Array:",heapsort(data.copy()))
+
